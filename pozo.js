0646434cefb0c59fcdf79dff6a88bffe58bd76e1
Omar Cordero <omar@gmail.com>
2021-02-26 13:57:02 -0500

    Se cambia los segundos e imágenes a la tinka

--- pozo.js
+++ pozo.js
@@ -1,23 +1,15 @@
 var content = document.getElementById('contenido');
 var content2 = document.getElementById('contenido2');
-var content3 = document.getElementById('contenido3');
 async function init() {
   await getUser();
+  content.style.display = "block";
+  setTimeout(() => {
+    content2.style.display = "none";
+  }, 0);
   setTimeout(() => {
     content.style.display = "none";
     content2.style.display = "block";
-    content3.style.display = "none";
-  }, 0);
-  setTimeout(() => {
-    content.style.display = "block";
-    content2.style.display = "none";
-    content3.style.display = "none";
-  }, 2500);
-  setTimeout(() => {
-    content.style.display = "none";
-    content2.style.display = "none";
-    content3.style.display = "block";
-  }, 5500);
+  }, 4000);
 }
 
 init();
